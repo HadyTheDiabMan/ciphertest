647bd867402de02a05c468f996ef726f86b5b443
hdiab22 <42975537+HadyTheDiabMan@users.noreply.github.com>
2019-04-01 12:18:59 -0400

    Add files via upload

---
+++ src/cip/Cipher.java
@@ -0,0 +1,19 @@
+package cip;
+
+public class Cipher 
+{
+
+	public static void main(String[] args) 
+	
+	{
+		String word= "subscribe to pewdiepie";
+		
+		for(int i=0;i<word.length();i++)
+		{
+			26-a
+		}
+		
+
+	}
+
+}
